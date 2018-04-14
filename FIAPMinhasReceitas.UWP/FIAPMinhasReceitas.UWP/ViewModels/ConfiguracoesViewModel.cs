07da8e39b709f0a14a3fdc6397a9a8a160c19154
Perfil <Perfil@DESKTOP-5PN4KON>
2018-04-14 15:26:48 -0300

    Implementado configurações

---
+++ FIAPMinhasReceitas.UWP/FIAPMinhasReceitas.UWP/ViewModels/ConfiguracoesViewModel.cs
@@ -0,0 +1,27 @@
+﻿using FIAPMinhasReceitas.Models.Abstracts;
+using FIAPMinhasReceitas.UWP.Services;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FIAPMinhasReceitas.UWP.ViewModels
+{
+    public class ConfiguracoesViewModel : NotifyableClass
+    {
+        private int? _ordemListaReceitasConfiguracao;
+
+        public int? OrdemListaReceitasConfiguracao
+        {
+            get
+            {
+                return StorageService.LerConfiguracao(StorageService.Configuracoes.OrdemListaReceitas, 0);
+            }
+            set
+            {
+                StorageService.GravarConfiguracao(StorageService.Configuracoes.OrdemListaReceitas, value);
+            }
+        }
+    }
+}
